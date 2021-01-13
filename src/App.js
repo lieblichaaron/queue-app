263abb1a4989af82f77df1af3c9247f624a93763
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-13 15:27:26 +0200

    create react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/ticket">
+          <Ticket />
+        </Route>
+        <Route path="/create">
+          <CreateLine />
+        </Route>
+        <Route path="/line">
+          <Line />
+        </Route>
+        <Route path="/">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
