0a5313f4997c5bb6267cec963f9907507b194849
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-14 20:52:55 +0200

    createline-wip, add form without location

--- src/components/create_line/createLine.jsx
+++ src/components/create_line/createLine.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 import styles from "./createLine.module.css";
 const CreateLine = () => {
+  const [serviceTimeOptions, setServiceTimeOptions] = useState([
+    ...Array(30).keys(),
+  ]);
   return (
     <div>
       <header className={styles.header}>
         <h3>Line Setup</h3>
       </header>
       <div className={styles["page-container"]}>
-        <p className="text-center">
+        <p className="text-center pb-3">
           Please enter the following information to start your queue.
         </p>
+        <Form>
+          <Form.Group controlId="storeName">
+            <Form.Label>Store name*</Form.Label>
+            <Form.Control type="text" placeholder="Enter store name" />
+          </Form.Group>
+
+          <Form.Group controlId="serviceTime">
+            <Form.Label>Average service time - per customer</Form.Label>
+            <Form.Control as="select">
+              {serviceTimeOptions.map((option) => (
+                <option key={option}>{option + 1 + "min"}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     </div>
   );
