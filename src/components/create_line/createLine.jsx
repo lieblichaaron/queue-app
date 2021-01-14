2908cf72013ab9d7305c1794b58b8850883566dc
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-14 20:16:21 +0200

    createline-wip, add page explination at top of page

--- src/components/create_line/createLine.jsx
+++ src/components/create_line/createLine.jsx
@@ -5,6 +5,11 @@ const CreateLine = () => {
       <header className={styles.header}>
         <h3>Line Setup</h3>
       </header>
+      <div className={styles["page-container"]}>
+        <p className="text-center">
+          Please enter the following information to start your queue.
+        </p>
+      </div>
     </div>
   );
 };
