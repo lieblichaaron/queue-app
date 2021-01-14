799f2e335a37caa016d48fb200f45e594d5f07c0
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-14 20:07:44 +0200

    createline-wip, add header

--- src/components/create_line/createLine.jsx
+++ src/components/create_line/createLine.jsx
@@ -1,5 +1,12 @@
+import styles from "./createLine.module.css";
 const CreateLine = () => {
-  return <div>create line</div>;
+  return (
+    <div>
+      <header className={styles.header}>
+        <h3>Line Setup</h3>
+      </header>
+    </div>
+  );
 };
 
 export default CreateLine;
