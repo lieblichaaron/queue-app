66338456fbd4d4c4f6c9b4494ef066438e73c25b
JakeRSA <jakenudels@gmail.com>
2021-01-16 16:01:37 +0200

    account-wip, implemented basic layout for account settings page

--- src/components/account/account.jsx
+++ src/components/account/account.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./account.css";
+import {
+  Container,
+  Row,
+  Button,
+  Col,
+  FormGroup,
+  FormLabel,
+} from "react-bootstrap";
+import { Formik, Form, Field } from "formik";
 
 function Account(props) {
+  const user = {
+    displayName: "Jake",
+    email: "jakenudels@gmail.com",
+  };
+
+  const [canEdit, setCanEdit] = useState(false);
+
   return (
     <div>
-      
+      <h2 className="w-100 py-3 px-1 text-center text-wrap">
+        Account Settings
+      </h2>
+      <Formik
+        initialValues={{
+          displayName: user.displayName,
+          email: user.email,
+        }}
+        enableReinitialize={true}
+        onSubmit={(values) => console.log(values)}
+      >
+        {(props) => (
+          <Form>
+            <Container fluid className="px-5">
+              <FormGroup className="account-form-group">
+                <FormLabel htmlFor="displayName">Display name</FormLabel>
+                <Field
+                  className={`form-input ${canEdit || "no-edit-field"}`}
+                  name="displayName"
+                />
+              </FormGroup>
+              <FormGroup className="account-form-fieldset">
+                <FormLabel htmlFor="email">Email address</FormLabel>
+                <Field
+                  className={`form-input ${canEdit || "no-edit-field"}`}
+                  name="email"
+                  type="email"
+                />
+              </FormGroup>
+              <Row className="d-flex justify-content-around mt-3">
+                {canEdit ? (
+                  <>
+                    <Col>
+                      <Button className="w-100" type="submit" value="submit">
+                        Confirm
+                      </Button>
+                    </Col>
+                    <Col>
+                      <Button
+                        className="w-100"
+                        type="reset"
+                        onClick={() => {
+                          props.resetForm();
+                          setCanEdit(false);
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                    </Col>
+                  </>
+                ) : (
+                  <Button
+                    className="w-100"
+                    type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+
+                      setCanEdit(true);
+                    }}
+                  >
+                    Update Information
+                  </Button>
+                )}
+              </Row>
+            </Container>
+          </Form>
+        )}
+      </Formik>
     </div>
-  )
+  );
 }
 
-export default Account
+export default Account;
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import Account from "../account/account";
+import { Link } from "react-router-dom";
 import LineList from "../line_list/LineList";
 import "./Dashboard.css";
 
@@ -30,16 +30,21 @@ function Dashboard() {
         Create a new line or manage existing lines at the press of a button
       </p>
       <span className="d-flex justify-content-center">
-        <Button className="create-line-btn py-4 px-5 my-5">Create a new line</Button>
+        <Link to="/create_line">
+          <Button className="create-line-btn py-4 px-5 my-5">
+            Create a new line
+          </Button>
+        </Link>
       </span>
       <div className="existing-lines-container p-2 pb-3 my-3">
         <h3 className="text-center">Current Lines</h3>
         <LineList lines={user.lines} />
       </div>
-      {/* <div className="account-settings-container">
-        <h3 className="text-center">Account Settings</h3>
-        <Account />
-      </div> */}
+      <span className="d-flex justify-content-center">
+        <Link to="/account">
+          <Button className="account-settings-btn">Account settings</Button>
+        </Link>
+      </span>
     </div>
   );
 }
