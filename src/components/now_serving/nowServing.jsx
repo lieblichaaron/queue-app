6731580677508e00df7d731f9e270eab97d3944d
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-16 19:06:22 +0200

    ticket-wip, create now serving component

--- src/components/now_serving/nowServing.jsx
+++ src/components/now_serving/nowServing.jsx
@@ -1,5 +1,12 @@
+import styles from "./nowServing.module.css";
 const NowServing = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.customer}>
+        NOW SERVING <span style={{ fontSize: "5rem" }}>#60</span>
+      </h1>
+    </div>
+  );
 };
 
 export default NowServing;
