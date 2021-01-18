ece888367fec48095130dbff1c1904e68afacb84
Julian lange <julian.laenge@gmail.com>
2021-01-18 23:34:28 +0200

    fixed sizes of images not consistent

--- src/components/about/carousel.jsx
+++ src/components/about/carousel.jsx
@@ -2,6 +2,7 @@ import { Carousel } from "react-bootstrap";
 
 const CarouselComponent = () => {
   const imagePath = `${process.env.PUBLIC_URL}/images/`;
+  const imageSize = { height: "12rem", width: "20rem" };
   return (
     <div className="mt-4">
       <Carousel>
@@ -10,6 +11,7 @@ const CarouselComponent = () => {
             className="d-block w-100"
             src={`${imagePath}People-standing-in-line.png`}
             alt="People In Line"
+            style={imageSize}
           />
           <Carousel.Caption>
             <h5 style={{ textShadow: "-2px -2px #14213D" }}>
@@ -22,6 +24,7 @@ const CarouselComponent = () => {
             className="d-block w-100"
             src={`${imagePath}Burger-Food-Truck.png`}
             alt="Food Truck"
+            style={imageSize}
           />
 
           <Carousel.Caption>
@@ -35,6 +38,7 @@ const CarouselComponent = () => {
             className="d-block w-100"
             src={`${imagePath}wooden-hut-shop.png`}
             alt="Wooden Hut Shop"
+            style={imageSize}
           />
 
           <Carousel.Caption>
