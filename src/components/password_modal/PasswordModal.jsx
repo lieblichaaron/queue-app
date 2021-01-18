8f74e1b93d1e06e39cc121d7976fcdd81985ec36
JakeRSA <jakenudels@gmail.com>
2021-01-18 12:45:33 +0200

    passwordmodal-wip, added basic validation

--- src/components/password_modal/PasswordModal.jsx
+++ src/components/password_modal/PasswordModal.jsx
@@ -7,72 +7,90 @@ import {
   Modal,
 } from "react-bootstrap";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 function PasswordModal(props) {
+  const { isOpen, onCloseModal } = props;
+
+  const validationSchema = Yup.object().shape({
+    oldPassword: Yup.string().required("Please enter your current password"),
+    newPassword: Yup.string().required("Please enter your new password"),
+    newPasswordConfirm: Yup.string()
+      .required("Please type your new password again")
+      .oneOf([Yup.ref("newPassword"), null], "Passwords do not match"),
+  });
+
   return (
-    <Modal show={props.isOpen} onHide={props.onCloseModal} centered>
+    <Modal show={isOpen} onHide={onCloseModal} centered>
       <Modal.Header closeButton>
         <Modal.Title>Change Password</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <Formik
-          initialValues={{
-            oldPassword: "",
-            newPassword: "",
-            newPasswordConfirm: "",
-          }}
-          onSubmit={(values) => {
-            console.log(values);
-          }}
-        >
+      <Formik
+        initialValues={{
+          oldPassword: "",
+          newPassword: "",
+          newPasswordConfirm: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={validationSchema}
+      >
+        {(props) => (
           <Form>
-            <Container fluid>
-              <FormGroup>
-                <FormLabel htmlFor="oldPassword">Old password</FormLabel>
-                <Field
-                  className="form-input"
-                  name="oldPassword"
-                  type="password"
-                />
-              </FormGroup>
-              <FormGroup>
-                <FormLabel htmlFor="newPassword">New password</FormLabel>
-                <Field
-                  className="form-input"
-                  name="newPassword"
-                  type="password"
-                />
-              </FormGroup>
-              <FormGroup>
-                <FormLabel htmlFor="newPasswordConfirm">
-                  Confirm new password
-                </FormLabel>
-                <Field
-                  className="form-input"
-                  name="newPasswordConfirm"
-                  type="password"
-                />
-              </FormGroup>
-            </Container>
+            <Modal.Body>
+              <Container fluid>
+                <FormGroup>
+                  <FormLabel htmlFor="oldPassword">Old password</FormLabel>
+                  <Field
+                    className="form-input"
+                    name="oldPassword"
+                    type="password"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="newPassword">New password</FormLabel>
+                  <Field
+                    className="form-input"
+                    name="newPassword"
+                    type="password"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="newPasswordConfirm">
+                    Confirm new password
+                  </FormLabel>
+                  <Field
+                    className="form-input"
+                    name="newPasswordConfirm"
+                    type="password"
+                  />
+                </FormGroup>
+              </Container>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                type="submit"
+                onClick={() => {
+                  onCloseModal();
+                }}
+                disabled={
+                  !props.isValid || Object.keys(props.touched).length === 0
+                }
+              >
+                Save changes
+              </Button>
+              <Button
+                onClick={() => {
+                  onCloseModal();
+                }}
+              >
+                Cancel
+              </Button>
+            </Modal.Footer>
           </Form>
-        </Formik>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          onClick={() => {
-            props.onCloseModal();
-          }}
-        >
-          Save changes
-        </Button>
-        <Button
-          onClick={() => {
-            props.onCloseModal();
-          }}
-        >
-          Cancel
-        </Button>
-      </Modal.Footer>
+        )}
+      </Formik>
     </Modal>
   );
 }
