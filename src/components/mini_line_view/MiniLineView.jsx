a870628cf30017e6f9eb02cc7445f3f1dea62477
JakeRSA <jakenudels@gmail.com>
2021-01-18 12:05:53 +0200

    dashboard-wip, made small changes to styling

--- src/components/mini_line_view/MiniLineView.jsx
+++ src/components/mini_line_view/MiniLineView.jsx
@@ -10,7 +10,7 @@ function MiniLineView(props) {
       <div>
         <p className="store-title mb-1">{store}</p>
         <p className="mb-1">
-          status: <span>{active ? "active" : "inactive"}</span>
+          status: <span className={active ? "green" : "red"}>{active ? "active" : "inactive"}</span>
         </p>
         <p className="mb-1">
           queue length: <span>{length}</span>
