e21814a896c75a3d7452c63dc1129ae260125293
Julian lange <julian.laenge@gmail.com>
2021-01-24 12:19:09 +0200

    connected 'get started today buttons with login modal

--- src/App.js
+++ src/App.js
@@ -7,57 +7,62 @@ import About from "./components/about/about";
 import Dashboard from "./components/dashboard/Dashboard";
 import Account from "./components/account/account";
 import CustomNavbar from "./components/navbar/customNavbar";
-import LoginModal from "./components/login/loginModal"
+import LoginModal from "./components/login/loginModal";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
-  
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isLoggedIn: false,
-      showLoginModal: false
-    }
+      showLoginModal: false,
+    };
   }
   manageLoginModal() {
-    this.setState({ showLoginModal: !this.state.showLoginModal })    
+    this.setState({ showLoginModal: !this.state.showLoginModal });
   }
 
   handleLogout() {
-    this.setState({isLoggedIn:false})
+    this.setState({ isLoggedIn: false });
   }
 
   render() {
     const { showLoginModal, isLoggedIn } = this.state;
-  return (
-    <Router>
-      <CustomNavbar isLoggedIn={isLoggedIn} handleSignIn={ () => this.manageLoginModal() }/>
-      
-      <Switch>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/ticket">
-          <TicketPage />
-        </Route>
-        <Route path="/account">
-          <Account />
-        </Route>
-        <Route path="/create">
-          <CreateLine />
-        </Route>
-        <Route path="/line">
-          <Line />
-        </Route>
-        <Route path="/">
-          <About />
-          <LoginModal showModal={showLoginModal} closeModal={ () => this.manageLoginModal() }/>
-        </Route>
-      </Switch>
-    </Router>
+    return (
+      <Router>
+        <CustomNavbar
+          isLoggedIn={isLoggedIn}
+          handleSignIn={() => this.manageLoginModal()}
+        />
+
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/ticket">
+            <TicketPage />
+          </Route>
+          <Route path="/account">
+            <Account />
+          </Route>
+          <Route path="/create">
+            <CreateLine />
+          </Route>
+          <Route path="/line">
+            <Line />
+          </Route>
+          <Route path="/">
+            <About handleSignIn={() => this.manageLoginModal()} />
+            <LoginModal
+              showModal={showLoginModal}
+              closeModal={() => this.manageLoginModal()}
+            />
+          </Route>
+        </Switch>
+      </Router>
     );
-    }
+  }
 }
 
 export default App;
--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,13 +3,17 @@ import Footer from "../footer/footer";
 import "./about.css";
 import CarouselComponent from "./carousel";
 
-const About = () => {
+const About = (props) => {
   return (
     <div className="text-white">
       <div className="d-flex flex-column align-items-center mt-5">
         <h1 className="mt-5">iQueue</h1>
         <h5 className="mt-4 ">Queue management made easy</h5>
-        <Button type="button" className="btn btn-primary mt-5 py-3 px-5">
+        <Button
+          type="button"
+          className="btn btn-primary mt-5 py-3 px-5"
+          onClick={(event) => props.handleSignIn(event)}
+        >
           Get started today
         </Button>
       </div>
@@ -17,12 +21,17 @@ const About = () => {
       <div className="what-we-do d-flex flex-column align-items-center pt-5 ">
         <h4 className="text-center">What we do</h4>
         <p className="px-4 mt-2 text-justify">
-          Why are queues still a thing in {new Date().getFullYear()}? <br /><br /> In an age
-          where customers expect instant service, nothing says "poor customer
-          experience" like a long line. Create a virtual queue and let your
-          customers scan a QR code to join the line. It's as simple as that.
+          Why are queues still a thing in {new Date().getFullYear()}? <br />
+          <br /> In an age where customers expect instant service, nothing says
+          "poor customer experience" like a long line. Create a virtual queue
+          and let your customers scan a QR code to join the line. It's as simple
+          as that.
         </p>
-        <Button type="button" className="btn btn-secondary mt-4 py-3 px-5">
+        <Button
+          type="button"
+          className="btn btn-secondary mt-4 py-3 px-5"
+          onClick={(event) => props.handleSignIn(event)}
+        >
           Get started today
         </Button>
       </div>
