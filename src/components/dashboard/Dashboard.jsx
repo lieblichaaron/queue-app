e444a996588cd9f0508a4aa7d2acfcee38dad727
JakeRSA <jakenudels@gmail.com>
2021-01-25 17:58:21 +0200

    api call to load lines on component mount

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,41 +1,26 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import UserContext from "../../contexts/UserContext";
 import LineList from "../line_list/LineList";
 import "./Dashboard.css";
 
 function Dashboard() {
+
+  const [lines, setLines] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
   const user = useContext(UserContext);
-  const lines = [
-    {
-      id: "600ed2a0c82668f8cafdc9ac",
-      ownerID: "600ecbad5d601d64b43cac9c",
-      isActive: true,
-      storeName: "Burger Bar",
-      estServiceTime: "3",
-      customerAnalytics: {
-        serviceTimes: [
-          { $numberInt: "2" },
-          { $numberInt: "5" },
-          { $numberInt: "3" },
-          { $numberInt: "8" },
-        ],
-        waitTimes: [
-          { $numberInt: "5" },
-          { $numberInt: "10" },
-          { $numberInt: "8" },
-          { $numberInt: "15" },
-        ],       
-      },
-      tickets: [1, 2, 3, 4, 5],
-      location: {
-        lat: "32.080",
-        lng: "34.785",
-        address: "123 Road Street, Tel Aviv",
-      },
-    },
-  ];
+
+  const getLines = async () => {
+    const res = await axios.get("http://localhost:5000" + "/line/owned-by/" + user.id)
+    setLines(res.data)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getLines();
+  }, [])
 
   return (
     <div>
@@ -54,7 +39,7 @@ function Dashboard() {
       </span>
       <div className="existing-lines-container p-2 pb-3 my-3">
         <h3 className="text-center black-text">Current Lines</h3>
-        <LineList lines={lines} />
+        {isLoading ? <p>loading...</p> : <LineList lines={lines} />}
       </div>
       <span className="d-flex justify-content-center">
         <Link to="/account">
