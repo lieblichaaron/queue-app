b005f403ca6cd54dc3792bf51739edae1d63cf67
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-26 13:33:03 +0200

    fix now serving

--- src/components/now_serving/nowServing.jsx
+++ src/components/now_serving/nowServing.jsx
@@ -1,10 +1,8 @@
 import styles from "./nowServing.module.css";
 const NowServing = ({ textColor, backgroundColor, currentCustomer }) => {
   return (
-    <div
-      className={styles.container}
-    >
-      <h1 className={styles.customer}>
+    <div className={styles.container} style={{ backgroundColor }}>
+      <h1 className={styles.customer} style={{ color: textColor }}>
         NOW SERVING <span style={{ fontSize: "5rem" }}>#{currentCustomer}</span>
       </h1>
     </div>
