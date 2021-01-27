cb062435c8422645c0faefccba8a0e67cf61353c
Julian lange <julian.laenge@gmail.com>
2021-01-27 13:28:43 +0200

    refactor of line page

--- src/components/line/line.jsx
+++ src/components/line/line.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
-import { Form, Button, Container, Badge } from "react-bootstrap";
+import { Form, Button, Badge } from "react-bootstrap";
 import Autocomplete from "react-google-autocomplete";
 import TitleBanner from "../title_banner/titleBanner";
-import Map from "../map/map";
+import MyMapComponent from "../map/map";
 import NowServing from "../now_serving/nowServing";
-import "./line.css";
 
 const Line = () => {
   const [address, setAddress] = useState();
@@ -21,19 +20,17 @@ const Line = () => {
     setLocationPicked(true);
   }, [lng]);
   return (
-    <Container className="m-0, p-0">
+    <div>
       <TitleBanner title="Line" />
-      <Container className="Container1">
-        <div className="p-3" style={{ color: "#ffffff" }}>
-          <h1 className="text-center pb-3">Current line size: {13} people.</h1>
+      <div className="p-3" style={{ color: "#ffffff" }}>
+        <h2 className="text-center pb-3">
+          Current line size: <br />
+          {13} people
+        </h2>
+        <div className="d-flex justify-content-center my-3">
+          <NowServing textColor="#14213d" backgroundColor="#e5e5e5" />
         </div>
-
-        <NowServing textColor="#14213d" backgroundColor="#e5e5e5" />
-      </Container>
-
-      <Container className="Buttons" style={{}}>
         <Button
-          className="Buttons1"
           style={{
             backgroundColor: "#14213d",
             color: "#14213d",
@@ -41,7 +38,6 @@ const Line = () => {
             marginTop: 10,
           }}
           className="w-100"
-          type="submit"
         >
           Next Customer
         </Button>
@@ -70,82 +66,107 @@ const Line = () => {
         >
           Delete queue
         </Button>
-      </Container>
-      <Container className="align-self-center">
-        <h1 className="d-flex justify-content-center">Customer Analytics</h1>
+        <div className="d-flex align-items-center text-white flex-column">
+          <h2 className="my-3">Customer Analytics</h2>
+          <div className="d-flex justify-content-center align-items-center">
+            <h5>Average Serving Time</h5>
+            <h3>
+              <Badge
+                className="ml-3"
+                style={{ color: "black", backgroundColor: "#fca318" }}
+                pill
+              >
+                5 Mins
+              </Badge>
+            </h3>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <h5>Estimated Time of Queue</h5>
+            <h3>
+              <Badge
+                className="ml-3"
+                pill
+                style={{ color: "black", backgroundColor: "#fca318" }}
+              >
+                5 Mins
+              </Badge>
+            </h3>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <h5>Average Waiting Time</h5>
+            <h3>
+              <Badge
+                className="ml-3"
+                pill
+                style={{ color: "black", backgroundColor: "#fca318" }}
+              >
+                5 Mins
+              </Badge>
+            </h3>
+          </div>
+        </div>
+        {/* onSubmit Update information */}
+        <Form>
+          <Form.Group controlId="storeName">
+            <Form.Label>Store name*</Form.Label>
+            <Form.Control
+              required
+              autoComplete="off"
+              type="text"
+              placeholder="Enter store name"
+              // onChange={handleStoreName}
+            />
+          </Form.Group>
 
-        <h4 className="d-flex justify-content-center">
-          Average Serving Time{" "}
-          <h3>
-            <Badge className="ml-3" variant="secondary" pill variant="warning">
-              5 Mins
-            </Badge>
-          </h3>
-        </h4>
-        <h4 className="d-flex justify-content-center">
-          Estimated Time of queue{" "}
-          <h3>
-            <Badge className="ml-3" variant="secondary" pill variant="warning">
-              5 Mins
-            </Badge>
-          </h3>
-        </h4>
-        <h4 className="d-flex justify-content-center pill">
-          Average Waiting Time{" "}
-          <h3>
-            <Badge className="ml-3" variant="secondary" pill variant="warning">
-              5 Mins
-            </Badge>
-          </h3>
-        </h4>
-      </Container>
-      <Form>
-        <Form.Group controlId="storeName">
-          <Form.Label>Store name*</Form.Label>
-          <Form.Control required type="text" placeholder="Enter store name" />
-        </Form.Group>
-      </Form>
-      <Autocomplete
-        style={{
-          width: "100%",
-          height: "calc(1.5em + .75rem + 2px)",
-          borderRadius: ".25rem",
-        }}
-        onPlaceSelected={(place) => {
-          setMap(place);
-        }}
-        types={["address"]}
-      />
-      <Container>
-        {locationPicked && (
-          <Map
-            lat={lat}
-            lng={lng}
-            address={address}
-            isMarkerShown
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyA0Kx9Y9puWzmvyo9yVW_fCZvAiDNnKhlA&v=3.exp&libraries=geometry,drawing,places`}
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `300px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
-        )}
-      </Container>
-      <Container>
-        <Button
-          className="Buttons1"
-          style={{
-            backgroundColor: "#14213d",
-            color: "#fca311",
-            border: "none",
-            marginTop: 10,
-          }}
-          className="w-100"
-          type="submit"
-        >
-          Update Information
-        </Button>
-      </Container>
-    </Container>
+          <Form.Group controlId="location">
+            <Form.Label>Store location*</Form.Label>
+
+            <Autocomplete
+              style={{
+                width: "100%",
+                height: "calc(1.5em + .75rem + 2px)",
+                borderRadius: ".25rem",
+              }}
+              onPlaceSelected={(place) => {
+                setMap(place);
+              }}
+              types={["address"]}
+            />
+            <MyMapComponent
+              lat={lat}
+              lng={lng}
+              address={address}
+              isMarkerShown={address}
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyA0Kx9Y9puWzmvyo9yVW_fCZvAiDNnKhlA&v=3.exp&libraries=geometry,drawing,places`}
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `300px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </Form.Group>
+
+          {/* Button to prevent implicit submission of the form  */}
+          <button
+            type="submit"
+            disabled
+            style={{ display: "none" }}
+            aria-hidden="true"
+          ></button>
+          <Button
+            style={{
+              backgroundColor: "#fca311",
+              color: "#14213d",
+              border: "none",
+              height: "2.5rem",
+            }}
+            className="w-100"
+            type="submit"
+            // disabled={buttonDisabled}
+          >
+            Update Information
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 };
 
